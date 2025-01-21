b762848be9200c95e907f12086540e3553af6dde
Fábio Augusto <fabio-augusto04@outlook.com>
2025-01-21 19:09:07 -0300

    chore: start Next.js project with shadcn/ui and Tailwind.css

---
+++ chat-ai/src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function home() {
+  return (
+    <div className="p-4">
+      <Button>Clique Aqui</Button>
+    </div>
+  );
+}
+
+export default home;
